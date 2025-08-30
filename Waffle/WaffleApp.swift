a4c63fdb3eee035937c1650059dc444b7b656107
Nick Molargik <nick@molargiksoftware.com>
2025-08-30 18:39:29 -0400

    Initial Commit

---
+++ Waffle/WaffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaffleApp.swift
+//  Waffle
+//
+//  Created by Nick Molargik on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WaffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
